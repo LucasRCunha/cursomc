1dc67dcca1b204d9a4ff914ee7b69714c0b1da45
LucasRCunha <lucasrcunha01@gmail.com>
2018-02-01 23:43:00 -0200

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/lucas/cursomc/domain/ItemPedido.java
+++ src/main/java/com/lucas/cursomc/domain/ItemPedido.java
@@ -5,22 +5,23 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
-public class ItemPedido implements Serializable{
+public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
-	
+
 	private Double desconto;
 	private Integer quantidade;
 	private Double preco;
-	
-	
-	public ItemPedido() {
-		
-	}
 
+	public ItemPedido() {
+
+	}
 
 	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
 		super();
@@ -31,55 +32,47 @@ public class ItemPedido implements Serializable{
 		this.preco = preco;
 	}
 
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
-	
+
 	public Produto getProduto() {
 		return id.getProduto();
 	}
-
-	
 
 	public ItemPedidoPK getId() {
 		return id;
 	}
 
-
 	public void setId(ItemPedidoPK id) {
 		this.id = id;
 	}
-
 
 	public Double getDesconto() {
 		return desconto;
 	}
 
-
 	public void setDesconto(Double desconto) {
 		this.desconto = desconto;
 	}
-
 
 	public Integer getQuantidade() {
 		return quantidade;
 	}
 
-
 	public void setQuantidade(Integer quantidade) {
 		this.quantidade = quantidade;
 	}
-
 
 	public Double getPreco() {
 		return preco;
 	}
 
-
 	public void setPreco(Double preco) {
 		this.preco = preco;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -87,7 +80,6 @@ public class ItemPedido implements Serializable{
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -106,6 +98,4 @@ public class ItemPedido implements Serializable{
 		return true;
 	}
 
-	
-	
 }
