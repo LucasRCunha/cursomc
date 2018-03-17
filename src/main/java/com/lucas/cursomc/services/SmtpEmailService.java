fe0fd1aaa8979b823206a91fc083285f1b129b3f
LucasRCunha <lucasrcunha01@gmail.com>
2018-03-17 16:30:33 -0300

    Email HTML

--- src/main/java/com/lucas/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/lucas/cursomc/services/SmtpEmailService.java
@@ -1,13 +1,18 @@
 package com.lucas.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractEmailService{
 
+	@Autowired
+	private JavaMailSender javaMailSender;
 	@Autowired
 	private MailSender mailSender;
 	
@@ -20,7 +25,15 @@ public class SmtpEmailService extends AbstractEmailService{
 		LOG.info("Enviando email...");
 		mailSender.send(msg);
 		LOG.info("Email enviado");
-		
+	}
+
+
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Enviando email...");
+		javaMailSender.send(msg);
+		LOG.info("Email enviado");
 	}
 
 }
